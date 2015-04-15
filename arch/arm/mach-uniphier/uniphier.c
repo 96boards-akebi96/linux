225096664bfe1cdfc79de85eddc4493de30245b4
Masahiro Yamada <yamada.masahiro@socionext.com>
2015-04-15 20:49:42 +0900

    @FIXME@ AIDET workaround
    Use GPIO irqchip.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>

--- arch/arm/mach-uniphier/uniphier.c
+++ arch/arm/mach-uniphier/uniphier.c
@@ -12,6 +12,10 @@
  * GNU General Public License for more details.
  */
 
+#include <linux/io.h>
+#include <linux/irqchip.h>
+#include <linux/of.h>
+#include <linux/of_address.h>
 #include <asm/mach/arch.h>
 
 static const char * const uniphier_dt_compat[] __initconst = {
@@ -25,6 +29,41 @@ static const char * const uniphier_dt_compat[] __initconst = {
 	NULL,
 };
 
+static const struct of_device_id uniphier_aidet_of_device_ids[] __initconst = {
+	{ .compatible = "socionext,uniphier-aidet" },
+	{ /* sentinel */ }
+};
+
+static void __init uniphier_init_irq(void)
+{
+	struct device_node *np;
+	void __iomem *reg;
+	u32 tmp;
+
+	reg = ioremap(0x55000090, 4); /* IRQCTL */
+	if (!reg)
+		goto err;
+	tmp = readl(reg);
+	tmp |= 0x000000ff;
+	iowrite32(tmp, reg);
+	iounmap(reg);
+
+	np = of_find_matching_node_and_match(NULL,
+					     uniphier_aidet_of_device_ids,
+					     NULL);
+	reg = of_iomap(np, 0);
+	if (!reg)
+		goto err;
+	tmp = readl(reg + 0x8);
+	tmp |= 0xff << 16;
+	writel(tmp, reg + 0x8);
+	iounmap(reg);
+
+err:
+	irqchip_init();
+}
+
 DT_MACHINE_START(UNIPHIER, "Socionext UniPhier")
 	.dt_compat	= uniphier_dt_compat,
+	.init_irq	= uniphier_init_irq,
 MACHINE_END
