d182167169d49272c50207f9ba1565a30370b1ba
Masahiro Yamada <yamada.masahiro@socionext.com>
2015-12-03 11:20:35 +0900

    clk: let of_clk_get_parent_name() fail for invalid clock-indices
    Currently, of_clk_get_parent_name() returns a wrong parent clock name
    when "clock-indices" property exists and the target index is not
    found in the property.  In this case, NULL should be returned.
    
    For example,
    
            oscillator {
                    compatible = "myclocktype";
                    #clock-cells = <1>;
                    clock-indices = <1>, <3>;
                    clock-output-names = "clka", "clkb";
            };
    
            consumer {
                    compatible = "myclockconsumer";
                    clocks = <&oscillator 0>, <&oscillator 1>;
            };
    
    Currently, of_clk_get_parent_name(consumer_np, 0) returns "clka"
    (and of_clk_get_parent_name(consumer_np, 1) also returns "clka",
    this is correct).   Because the "clock-indices" in the clock parent
    does not contain <0>, of_clk_get_parent_name(consumer_np, 0) should
    return NULL.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- drivers/clk/clk.c
+++ drivers/clk/clk.c
@@ -3086,6 +3086,9 @@ const char *of_clk_get_parent_name(struct device_node *np, int index)
 		}
 		count++;
 	}
+	/* We went off the end of 'clock-indices' without finding it */
+	if (prop && !vp)
+		return NULL;
 
 	if (of_property_read_string_index(clkspec.np, "clock-output-names",
 					  index,
