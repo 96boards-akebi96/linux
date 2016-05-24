2c1dbb51ff50fdd95e3f3c123598b81ee1f4733b
Kunihiko Hayashi <hayashi.kunihiko@socionext.com>
2016-05-24 20:57:07 +0900

    thermal: uniphier: add support for LD20, and refactor codes to apply multiple SoC
    This patch modifies codes to refactor the following functions,
    - remove unused definitions
    - add error recovery codes
    - add SoC specified data to apply multiple SoC.
    This driver supports ProXstream2 and LD20 that have a thermal sensor in UniPhier SoC.
    
    Signed-off-by: Kunihiko Hayashi <hayashi.kunihiko@socionext.com>

--- drivers/thermal/uniphier_thermal.c
+++ drivers/thermal/uniphier_thermal.c
@@ -23,6 +23,7 @@
 #include <linux/io.h>
 #include <linux/module.h>
 #include <linux/of.h>
+#include <linux/of_platform.h>
 #include <linux/platform_device.h>
 
 #include <linux/thermal.h>
@@ -30,51 +31,30 @@
 
 #include "thermal_core.h"
 
-#define PMPVTCTLENA_OFFSET			0x00000000
-#define PMPVTCTLENA_PMPVTCTLEN_A		(0x1 << 0)
-#define PMPVTCTLENA_PMPVTCTLEN_A_STOP		(0x0 << 0)
-#define PMPVTCTLENA_PMPVTCTLEN_A_START		(0x1 << 0)
+/* block */
+#define PMPVTCTLEN_OFFSET			0x00000000
+#define PMPVTCTLEN_PMPVTCTLEN			(0x1 << 0)
+#define PMPVTCTLEN_PMPVTCTLEN_STOP		(0x0 << 0)
+#define PMPVTCTLEN_PMPVTCTLEN_START		(0x1 << 0)
 
-#define PMPVTCTLMODEA_OFFSET			0x00000004
-#define PMPVTCTLMODEA_PMPVTCTLMODE_A		(0xf << 0)
-#define PMPVTCTLMODEA_PMPVTCTLMODE_A_TEMPMON	(0x5 << 0)
+#define PMPVTCTLMODE_OFFSET			0x00000004
+#define PMPVTCTLMODE_PMPVTCTLMODE		(0xf << 0)
+#define PMPVTCTLMODE_PMPVTCTLMODE_TEMPMON	(0x5 << 0)
 
-#define TCOUNTA_OFFSET				0x00000018
-#define TCOUNTA_V_TCOUNT_A			(0x3ff << 0)
+#define EMONREPEAT_OFFSET			0x00000040
+#define EMONREPEAT_EMONENDLESS			(0x1 << 24)
+#define EMONREPEAT_EMONENDLESS_ENABLE		(0x1 << 24)
+#define EMONREPEAT_EMONPERIOD			(0xf << 0)
+#define EMONREPEAT_EMONPERIOD_1000000		(0x9 << 0)
 
-#define EMONREPEATA_OFFSET			0x00000040
-#define EMONREPEATA_EMONENDLESS_A		(0x1 << 24)
-#define EMONREPEATA_EMONENDLESS_A_DISABLE	(0x0 << 24)
-#define EMONREPEATA_EMONENDLESS_A_ENABLE	(0x1 << 24)
-#define EMONREPEATA_EMONNUM_A			(0xfff << 8)
-#define EMONREPEATA_EMONNUM_A_1TIME		(0x1 << 8)
-#define EMONREPEATA_EMONPERIOD_A		(0xf << 0)
-#define EMONREPEATA_EMONPERIOD_A_1000000	(0x9 << 0)
-
+/* common */
 #define PMPVTCTLMODESEL_OFFSET			0x00000900
-#define PMPVTCTLMODESEL_PMPVTCTLMODESEL		(0x7 << 0)
-#define PMPVTCTLMODESEL_PMPVTCTLMODESEL_MAIN	(0x1 << 0)
-
-#define SETFANPARAM1_OFFSET			0x00000904
-#define SETFANPARAM1_ETMODINTERCEPTSEL		(0x1 << 30)
-#define SETFANPARAM1_ETMODINTERCEPTSEL_EFUSE	(0x0 << 30)
-#define SETFANPARAM1_ETMODINTERCEPTSEL_ETMODINTERCEPT \
-						(0x1 << 30)
-#define SETFANPARAM1_ETMODINTERCEPT		(0x3fff << 16)
-#define SETFANPARAM1_ETMODSLOPESEL		(0x1 << 15)
-#define SETFANPARAM1_ETMODSLOPESEL_EFUSE	(0x0 << 15)
-#define SETFANPARAM1_ETMODSLOPESEL_ETMODSLOPE	(0x1 << 15)
-#define SETFANPARAM1_ETMODSLOPE			(0x7fff << 0)
 
 #define SETALERT0_OFFSET			0x00000910
 #define SETALERT1_OFFSET			0x00000914
 #define SETALERT2_OFFSET			0x00000918
-#define SETALERT_EALERTTEMP0			(0xff << 8)
-#define SETALERT_EOFUFSEL0			(0x1 << 4)
-#define SETALERT_EOFUFSEL0_HIGHER		(0x0 << 4)
-#define SETALERT_EOFUFSEL0_LOWER		(0x1 << 4)
+#define SETALERT_EALERTTEMP0_OF			(0xff << 16)
 #define SETALERT_EALERTEN0			(0x1 << 0)
-#define SETALERT_EALERTEN0_DISUSE		(0x0 << 0)
 #define SETALERT_EALERTEN0_USE			(0x1 << 0)
 
 #define PMALERTINTCTL_OFFSET			0x00000920
@@ -87,25 +67,32 @@
 #define TMOD_V_TMOD				(0x1ff << 0)
 
 #define TMODCOEF_OFFSET				0x00000e5c
-#define TMODCOEF_V_TMODINTERCEPT		(0x3fff << 16)
-#define TMODCOEF_V_TMODSLOPE			(0x7fff << 0)
 
-#define SETFANPARAM1_ETMODINTERCEPT_SETTINGS	(0x0f86 << 16)
-#define SETFANPARAM1_ETMODSLOPE_SETTINGS	(0x6844 << 0)
-
+/* param */
 #define TEMP_LIMIT_DEFAULT			(95 * 1000)	 /* Default is 95 degrees Celsius */
 #define ALERT_CH_NUM				3
 
-struct uniphier_thermal_dev {
-	struct device		*dev;
-	void __iomem		*base;
+struct uniphier_thermal_priv_t;
 
+struct uniphier_thermal_dev {
+	struct device *dev;
+	void __iomem  *base;
+	int irq;
+
+	u32 alert_en[ALERT_CH_NUM];
 	struct thermal_zone_device *tz_dev;
-	u32                     alert_en[ALERT_CH_NUM];
+	struct uniphier_thermal_zone *zone;
+	struct uniphier_thermal_priv_t *priv;
 };
 
 struct uniphier_thermal_zone {
-	void __iomem		*base;
+	void __iomem *base;
+};
+
+struct uniphier_thermal_priv_t {
+	u32 block_offset;
+	u32 setup_address;
+	u32 setup_value;
 };
 
 static inline void maskwritel(void __iomem *base, u32 offset, u32 mask, u32 v)
@@ -118,47 +105,51 @@ static inline void maskwritel(void __iomem *base, u32 offset, u32 mask, u32 v)
 
 static inline u32 maskreadl(void __iomem *base, u32 offset, u32 mask)
 {
-	return (readl(base + offset) & mask) * 1000;	/* millicelsius */
+	return (readl_relaxed(base + offset) & mask);
 }
+
+/* for PXs2 */
+static const struct uniphier_thermal_priv_t uniphier_thermal_priv_data_pxs2 = {
+	.block_offset  = 0x000,
+	.setup_address = 0x904,
+	.setup_value   = 0x4f86e844,
+};
+
+/* for LD20 */
+static const struct uniphier_thermal_priv_t uniphier_thermal_priv_data_ld20 = {
+	.block_offset  = 0x800,
+	.setup_address = 0x938,
+	.setup_value   = 0x4f22e8ee,
+};
 
 static int uniphier_thermal_initialize_sensor(struct uniphier_thermal_dev *tdev)
 {
 	void __iomem *base = tdev->base;
 
 	/* stop PVT control */
-	maskwritel(base, PMPVTCTLENA_OFFSET,
-		    PMPVTCTLENA_PMPVTCTLEN_A, PMPVTCTLENA_PMPVTCTLEN_A_STOP);
+	maskwritel(base, tdev->priv->block_offset + PMPVTCTLEN_OFFSET,
+		    PMPVTCTLEN_PMPVTCTLEN, PMPVTCTLEN_PMPVTCTLEN_STOP);
 
 	/* set up default if missing eFuse */
 	if (readl(base + TMODCOEF_OFFSET) == 0x0) {
-		maskwritel(base, SETFANPARAM1_OFFSET,
-			    SETFANPARAM1_ETMODINTERCEPTSEL |
-			      SETFANPARAM1_ETMODINTERCEPT |
-			      SETFANPARAM1_ETMODSLOPESEL |
-			      SETFANPARAM1_ETMODSLOPE,
-			    SETFANPARAM1_ETMODINTERCEPTSEL_ETMODINTERCEPT |
-			      SETFANPARAM1_ETMODINTERCEPT_SETTINGS |
-			      SETFANPARAM1_ETMODSLOPESEL_ETMODSLOPE |
-			      SETFANPARAM1_ETMODSLOPE_SETTINGS);
+		maskwritel(base, tdev->priv->setup_address, ((u32)~0),
+			   tdev->priv->setup_value);
 	}
 
 	/* set mode of temperature monitor */
-	maskwritel(base, PMPVTCTLMODEA_OFFSET,
-		    PMPVTCTLMODEA_PMPVTCTLMODE_A,
-		    PMPVTCTLMODEA_PMPVTCTLMODE_A_TEMPMON);
+	maskwritel(base, tdev->priv->block_offset + PMPVTCTLMODE_OFFSET,
+		    PMPVTCTLMODE_PMPVTCTLMODE,
+		    PMPVTCTLMODE_PMPVTCTLMODE_TEMPMON);
 
 	/* set period (ENDLESS, 100ms) */
-	maskwritel(base, EMONREPEATA_OFFSET,
-		    EMONREPEATA_EMONENDLESS_A |
-		      EMONREPEATA_EMONNUM_A |
-		      EMONREPEATA_EMONPERIOD_A,
-		    EMONREPEATA_EMONENDLESS_A_ENABLE |
-		      EMONREPEATA_EMONPERIOD_A_1000000);
+	maskwritel(base, tdev->priv->block_offset + EMONREPEAT_OFFSET,
+		    EMONREPEAT_EMONENDLESS |
+		    EMONREPEAT_EMONPERIOD,
+		    EMONREPEAT_EMONENDLESS_ENABLE |
+		    EMONREPEAT_EMONPERIOD_1000000);
 
-	/* set UNIT_A(Main) */
-	maskwritel(base, PMPVTCTLMODESEL_OFFSET,
-		    PMPVTCTLMODESEL_PMPVTCTLMODESEL,
-		    PMPVTCTLMODESEL_PMPVTCTLMODESEL_MAIN);
+	/* set mode select */
+	maskwritel(base, PMPVTCTLMODESEL_OFFSET, ((u32)~0), 0);
 
 	return 0;
 }
@@ -170,13 +161,12 @@ static int uniphier_thermal_set_limit(struct uniphier_thermal_dev *tdev, u32 ch,
 	if (ch >= ALERT_CH_NUM)
 		return -EINVAL;
 
-	/* set alert(ch) temperature */
+	/* set alert temperature */
 	maskwritel(base, SETALERT0_OFFSET + (ch << 2),
-		    SETALERT_EALERTTEMP0 |
-		      SETALERT_EOFUFSEL0 |
-		      SETALERT_EALERTEN0,
-		    SETALERT_EALERTEN0_USE |
-		      (temp_limit << 8));
+		   SETALERT_EALERTEN0 |
+		   SETALERT_EALERTTEMP0_OF,
+		   SETALERT_EALERTEN0_USE |
+		   ((temp_limit / 1000) << 16));
 
 	return 0;
 }
@@ -194,8 +184,8 @@ static int uniphier_thermal_enable_sensor(struct uniphier_thermal_dev *tdev)
 	maskwritel(base, PMALERTINTCTL_OFFSET, PMALERTINTCTL_ALL_BITS, bits);
 
 	/* start PVT control */
-	maskwritel(base, PMPVTCTLENA_OFFSET,
-		    PMPVTCTLENA_PMPVTCTLEN_A, PMPVTCTLENA_PMPVTCTLEN_A_START);
+	maskwritel(base, tdev->priv->block_offset + PMPVTCTLEN_OFFSET,
+		    PMPVTCTLEN_PMPVTCTLEN, PMPVTCTLEN_PMPVTCTLEN_START);
 
 	return 0;
 }
@@ -208,22 +198,19 @@ static int uniphier_thermal_disable_sensor(struct uniphier_thermal_dev *tdev)
 	maskwritel(base, PMALERTINTCTL_OFFSET, PMALERTINTCTL_ALL_BITS, 0);
 
 	/* stop PVT control */
-	maskwritel(base, PMPVTCTLENA_OFFSET,
-		    PMPVTCTLENA_PMPVTCTLEN_A, PMPVTCTLENA_PMPVTCTLEN_A_STOP);
+	maskwritel(base, tdev->priv->block_offset + PMPVTCTLEN_OFFSET,
+		    PMPVTCTLEN_PMPVTCTLEN, PMPVTCTLEN_PMPVTCTLEN_STOP);
 
 	return 0;
-}
-
-static int __uniphier_thermal_get_temp(void __iomem *base)
-{
-	return maskreadl(base, TMOD_OFFSET, TMOD_V_TMOD);
 }
 
 static int uniphier_thermal_get_temp(void *data, int *out_temp)
 {
 	struct uniphier_thermal_zone *zone = data;
+	u32 temp;
 
-	*out_temp = __uniphier_thermal_get_temp(zone->base);
+	temp = maskreadl(zone->base, TMOD_OFFSET, TMOD_V_TMOD);
+	*out_temp = temp * 1000;	/* millicelsius */
 
 	return 0;
 }
@@ -262,6 +249,8 @@ static irqreturn_t uniphier_thermal_alarm_handler(int irq, void *_tdev)
 	return IRQ_HANDLED;
 }
 
+static const struct of_device_id uniphier_thermal_dt_ids[];
+
 static int uniphier_thermal_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
@@ -269,49 +258,68 @@ static int uniphier_thermal_probe(struct platform_device *pdev)
 	struct uniphier_thermal_dev *tdev;
 	struct uniphier_thermal_zone *zone;
 	const struct thermal_trip *trips;
+	const struct of_device_id *of_id;
 	int i, ret, irq;
 	u32 temp_limit = TEMP_LIMIT_DEFAULT;
 
+	of_id = of_match_device(uniphier_thermal_dt_ids, dev);
+	if (!of_id) {
+		dev_err(dev, "failed to probe\n");
+		return -EINVAL;
+	}
+
 	tdev = devm_kzalloc(dev, sizeof(*tdev), GFP_KERNEL);
-	if (!tdev)
+	if (!tdev) {
+		dev_err(dev, "failed to allocate memory for driver\n");
 		return -ENOMEM;
+	}
 
 	platform_set_drvdata(pdev, tdev);
 
 	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
 	tdev->base = devm_ioremap_resource(dev, res);
-	if (IS_ERR(tdev->base))
-		return PTR_ERR(tdev->base);
+	if (IS_ERR(tdev->base)) {
+		dev_err(dev, "failed to map resource\n");
+		ret = PTR_ERR(tdev->base);
+		goto err_tdev;
+	}
 
 	tdev->dev = dev;
+	tdev->priv = (struct uniphier_thermal_priv_t *)of_id->data;
 
 	irq = platform_get_irq(pdev, 0);
 	if (irq < 0) {
 		dev_err(dev, "failed to get irq\n");
-		return -EINVAL;
+		ret = -EINVAL;
+		goto err_map;
 	}
+
+	tdev->irq = irq;
 
 	/* register sensor */
 	zone = devm_kzalloc(dev, sizeof(*zone), GFP_KERNEL);
 	if (!zone) {
-		dev_err(dev, "failed to allocate memory\n");
-		return -ENOMEM;
+		dev_err(dev, "failed to allocate memory for sensor\n");
+		ret = -ENOMEM;
+		goto err_map;
 	}
 
 	zone->base = tdev->base;
+	tdev->zone = zone;
 	tdev->tz_dev = thermal_zone_of_sensor_register(dev, 0, zone,
 					     &uniphier_of_thermal_ops);
 	if (IS_ERR(tdev->tz_dev)) {
 		dev_err(dev, "failed to register thermal zone\n");
-		return PTR_ERR(tdev->tz_dev);
+		ret = PTR_ERR(tdev->tz_dev);
+		goto err_zone;
 	}
 
 	/* get trips */
 	trips = of_thermal_get_trip_points(tdev->tz_dev);
 	if (of_thermal_get_ntrips(tdev->tz_dev) > ALERT_CH_NUM) {
-		dev_err(dev, "ther number of trips exceeds %d\n", ALERT_CH_NUM);
+		dev_err(dev, "the number of trips exceeds %d\n", ALERT_CH_NUM);
 		ret = -EINVAL;
-		goto err;
+		goto err_sensor;
 	}
 	for (i = 0; i < of_thermal_get_ntrips(tdev->tz_dev); i++) {
 		if (trips[i].type == THERMAL_TRIP_CRITICAL) {
@@ -322,7 +330,7 @@ static int uniphier_thermal_probe(struct platform_device *pdev)
 	if (i == of_thermal_get_ntrips(tdev->tz_dev)) {
 		dev_err(dev, "failed to find critical trip\n");
 		ret = -EINVAL;
-		goto err;
+		goto err_sensor;
 	}
 
 	/* initialize sensor */
@@ -335,11 +343,11 @@ static int uniphier_thermal_probe(struct platform_device *pdev)
 		tdev->alert_en[i] = 1;
 	}
 
-	ret = devm_request_irq(dev, irq, uniphier_thermal_alarm_handler,
-			       0, "uniphier_thermal", tdev);
+	ret = devm_request_irq(dev, tdev->irq, uniphier_thermal_alarm_handler,
+			       0, "thermal", tdev);
 	if (ret) {
-		dev_err(dev, "failed to execute request_irq(%d)\n", res->start);
-		goto err;
+		dev_err(dev, "failed to execute request_irq\n");
+		goto err_sensor;
 	}
 
 	/* enable sensor */
@@ -350,8 +358,14 @@ static int uniphier_thermal_probe(struct platform_device *pdev)
 
 	return 0;
 
-err:
+err_sensor:
 	thermal_zone_of_sensor_unregister(dev, tdev->tz_dev);
+err_zone:
+	devm_kfree(dev, tdev->zone);
+err_map:
+	devm_iounmap(dev, tdev->base);
+err_tdev:
+	devm_kfree(dev, tdev);
 
 	return ret;
 }
@@ -361,19 +375,30 @@ static int uniphier_thermal_remove(struct platform_device *pdev)
 	struct device *dev = &pdev->dev;
 	struct uniphier_thermal_dev *tdev = platform_get_drvdata(pdev);
 
-	thermal_zone_of_sensor_unregister(dev, tdev->tz_dev);
-
 	/* disable sensor */
 	uniphier_thermal_disable_sensor(tdev);
+
+	devm_free_irq(dev, tdev->irq, tdev);
+	thermal_zone_of_sensor_unregister(dev, tdev->tz_dev);
+	devm_kfree(dev, tdev->zone);
+	devm_iounmap(dev, tdev->base);
+	devm_kfree(dev, tdev);
 
 	return 0;
 }
 
 static const struct of_device_id uniphier_thermal_dt_ids[] = {
-	{ .compatible = "socionext,uniphier-thermal" },
+	{
+		.compatible = "socionext,proxstream2-thermal",
+		.data       = (void *)&uniphier_thermal_priv_data_pxs2,
+	},
+	{
+		.compatible = "socionext,ph1-ld20-thermal",
+		.data       = (void *)&uniphier_thermal_priv_data_ld20,
+	},
 	{ /* sentinel */ }
 };
-MODULE_DEVICE_TABLE(of, uniphier_thermal_of_match);
+MODULE_DEVICE_TABLE(of, uniphier_thermal_dt_ids);
 
 static struct platform_driver uniphier_thermal_driver = {
 	.probe = uniphier_thermal_probe,
