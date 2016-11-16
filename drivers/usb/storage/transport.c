e343f659d7959c47175b125da55029c27ad0ae63
Motoya Tanigawa <tanigawa.motoya@socionext.com>
2016-11-16 23:53:38 +0900

    usb: storage: add workaround for HDD inquiry problem
    Signed-off-by: Motoya Tanigawa <tanigawa.motoya@socionext.com>

--- drivers/usb/storage/transport.c
+++ drivers/usb/storage/transport.c
@@ -1047,6 +1047,11 @@ int usb_stor_Bulk_max_lun(struct us_data *us)
 	 */
 	if (result > 0) {
 		if (us->iobuf[0] < 16) {
+
+			dev_info(&us->pusb_intf->dev,
+				"USB Storage Bulk Only Transport Max LUN Success.\n");
+			msleep(3000);
+
 			return us->iobuf[0];
 		} else {
 			dev_info(&us->pusb_intf->dev,
