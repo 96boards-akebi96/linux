0da2729fe67103938f0918aff9691c356bc3fb44
Kunihiko Hayashi <hayashi.kunihiko@socionext.com>
2018-08-22 21:13:01 +0900

    [backport 16af2d65842d] ata: add an extra argument to ahci_platform_get_resources()
    Add an extra argument to ahci_platform_get_resources(), that is
    for the bitmap representing the resource to get in this function.
    
    Currently there is no resources to be defined, so all the callers set
    '0' to the argument.
    
    Suggested-by: Hans de Goede <hdegoede@redhat.com>
    Cc: Thierry Reding <thierry.reding@gmail.com>
    Cc: Matthias Brugger <matthias.bgg@gmail.com>
    Cc: Patrice Chotard <patrice.chotard@st.com>
    Cc: Maxime Ripard <maxime.ripard@free-electrons.com>
    Signed-off-by: Kunihiko Hayashi <hayashi.kunihiko@socionext.com>
    Reviewed-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- drivers/ata/ahci_da850.c
+++ drivers/ata/ahci_da850.c
@@ -73,7 +73,7 @@ static int ahci_da850_probe(struct platform_device *pdev)
 	void __iomem *pwrdn_reg;
 	int rc;
 
-	hpriv = ahci_platform_get_resources(pdev);
+	hpriv = ahci_platform_get_resources(pdev, 0);
 	if (IS_ERR(hpriv))
 		return PTR_ERR(hpriv);
 
